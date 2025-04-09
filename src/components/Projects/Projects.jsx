0bb80626c792e5aa39cf468fc6775ef4d6ce1885
hamzaelboukri <hamzaelboukri01@gmail.com>
2025-04-09 12:33:56 +0100

    add image to web

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,64 +1,65 @@
-import React, { useEffect, useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import ProjectCard from "./ProjectCards";
-import Particle from "../Particle";
-import Spinner from "react-bootstrap/Spinner";
+  import React, { useEffect, useState } from "react";
+  import { Container, Row, Col } from "react-bootstrap";
+  import ProjectCard from "./ProjectCards";
+  import Particle from "../Particle";
+  import Spinner from "react-bootstrap/Spinner";
 
-function Projects() {
-  const [projects, setProjects] = useState([]);
-  const [loading, setLoading] = useState(true);
+  function Projects() {
+    const [projects, setProjects] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const apiUrl = "https://api.jsonbin.io/v3/b/67f58a3a8561e97a50fb546e 	";
-   
+    useEffect(() => {
+      const apiUrl = "https://api.jsonbin.io/v3/b/67f62f8a8561e97a50fba323";
+        
+    
 
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        // 数据在 `record` 字段中
-        setProjects(data.record);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching projects:", error);
-        setLoading(false);
-      });
-  }, []);
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+      
+          setProjects(data.record);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error("Error fetching projects:", error);
+          setLoading(false);
+        });
+    }, []);
 
-  return (
-    <Container fluid className="project-section">
-      <Particle />
-      <Container>
-        <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
-        </h1>
-        <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
-        </p>
-        {loading ? (
-          <div className="text-center">
-            <Spinner animation="border" role="status">
-              <span className="visually-hidden">Loading...</span>
-            </Spinner>
-          </div>
-        ) : (
-          <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-            {projects.map((project, index) => (
-              <Col md={4} key={index} className="project-card">
-                <ProjectCard
-                  imgPath={project.imgPath}
-                  isBlog={false}
-                  title={project.title}
-                  description={project.description}
-                  ghLink={project.ghLink}
-                />
-              </Col>
-            ))}
-          </Row>
-        )}
+    return (
+      <Container fluid className="project-section">
+        <Particle />
+        <Container>
+          <h1 className="project-heading">
+            My Recent <strong className="purple">Works </strong>
+          </h1>
+          <p style={{ color: "white" }}>
+            Here are a few projects I've worked on recently.
+          </p>
+          {loading ? (
+            <div className="text-center">
+              <Spinner animation="border" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </Spinner>
+            </div>
+          ) : (
+            <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+              {projects.map((project, index) => (
+                <Col md={4} key={index} className="project-card">
+                  <ProjectCard
+                    imgPath={project.imgPath}
+                    isBlog={false}
+                    title={project.title}
+                    description={project.description}
+                    ghLink={project.ghLink}
+                  />
+                </Col>
+              ))}
+            </Row>
+          )}
+        </Container>
       </Container>
-    </Container>
-  );
-}
+    );
+  }
 
-export default Projects;
+  export default Projects;
